580e8abb3c03c4bd46e3a3f2d3c52e60454811ac
Lukasz Wojtach <circusmagnus@gmail.com>
2017-12-09 02:24:13 +0100

    SQLite basic complete

--- app/src/main/java/pl/wojtach/listazakupow/details/stateHandling.kt
+++ app/src/main/java/pl/wojtach/listazakupow/details/stateHandling.kt
@@ -1,10 +1,16 @@
 package pl.wojtach.listazakupow.details
 
+import android.content.Context
 import pl.wojtach.listazakupow.shared.ShoppingList
+import pl.wojtach.listazakupow.shared.getAllShoppingListsFromDb
 
 fun initialState() = { ShoppingList(
         name = "Nowa lista",
         timestamp = System.currentTimeMillis(),
         shoppingItems = emptyList())}
+
+fun getAllShoppingLists(): (Context) -> List<ShoppingList> =
+        {context -> getAllShoppingListsFromDb(context)
+                .mapToState
 
 fun draw(state: )
--- app/src/main/java/pl/wojtach/listazakupow/shared/dbReaders.kt
+++ app/src/main/java/pl/wojtach/listazakupow/shared/dbReaders.kt
@@ -1,38 +1,58 @@
 package pl.wojtach.listazakupow.shared
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
+import android.provider.BaseColumns
 import pl.wojtach.listazakupow.database.DbContract
+import pl.wojtach.listazakupow.database.DbContract.ShoppingItemsTable.Columns.shoppingItem
+import pl.wojtach.listazakupow.database.DbContract.ShoppingItemsTable.Columns.shoppingListId
+import pl.wojtach.listazakupow.database.DbContract.ShoppingListsTable.Columns.name
+import pl.wojtach.listazakupow.database.DbContract.ShoppingListsTable.Columns.timestamp
 import pl.wojtach.listazakupow.database.DbHelper
 
 val shoppingListsProjection = arrayOf(
-        DbContract.ShoppingListsTable.Columns.name,
-        DbContract.ShoppingListsTable.Columns.timestamp
+        BaseColumns._ID,
+        name,
+        timestamp
 )
 
 val shoppingItemsProjection = arrayOf(
-        DbContract.ShoppingItemsTable.Columns.shoppingItem,
-        DbContract.ShoppingItemsTable.Columns.shoppingListId
+        BaseColumns._ID,
+        shoppingItem,
+        shoppingListId
 )
 
-val select
+val selectAllShoppingLists = "SELECT * FROM ${DbContract.ShoppingListsTable} ORDER BY $timestamp"
 
-fun getAllShoppingListsFromDb(context: Context): List<ShoppingList> =
-//        DbHelper(context).readableDatabase.query(
-//                DbContract.ShoppingListsTable.name,
-//                shoppingListsProjection,
-//                null,
-//                null,
-//                null,
-//                null,
-//                DbContract.ShoppingListsTable.Columns.timestamp
-//        )
-
-DbHelper(context).readableDatabase
-        .rawQuery("SELECT * FROM ${DbContract.ShoppingListsTable}", null)
+fun getAllShoppingListsFromDb(context: Context): List<ShoppingList> = DbHelper(context).readableDatabase
+        .rawQuery(selectAllShoppingLists, null)
         .let { mapToEntities(it) }
 
-fun mapToEntities(it: Cursor): List<ShoppingList> {
-    while (it.moveToNext())
-        it.getInt()
+fun mapToEntities(cursor: Cursor): List<ShoppingList> {
+
+    tailrec fun addToList(list: MutableList<ShoppingList>) : MutableList<ShoppingList> =
+        if(!cursor.moveToNext()) list
+        else {
+            list.add(ShoppingList(
+                    name = cursor.getString(getIndex(name, cursor)),
+                   timestamp = cursor.getLong(getIndex(timestamp, cursor)),
+                    shoppingItems = emptyList())
+            )
+            addToList(list)
+        }
+
+    return addToList(mutableListOf()).also { cursor.close() }
 }
+
+fun getIndex(columnName: String, cursor: Cursor) = cursor.getColumnIndexOrThrow(columnName)
+
+fun writeShoppingList(context: Context, shoppingList: ShoppingList) = DbHelper(context)
+        .writableDatabase
+        .insert(DbContract.ShoppingListsTable.name, null, getContentValues(shoppingList))
+
+fun getContentValues(shoppingList: ShoppingList): ContentValues =
+        ContentValues().apply {
+            put(name, shoppingList.name)
+            put(timestamp, shoppingList.timestamp)
+        }
