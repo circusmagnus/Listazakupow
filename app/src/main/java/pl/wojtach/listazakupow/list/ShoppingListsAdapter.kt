928d1923965b313d708a4865b6720f061103bfc4
Lukasz Wojtach <circusmagnus@gmail.com>
2017-12-10 02:48:33 +0100

    Fix inflating item view on list

--- app/src/main/java/pl/wojtach/listazakupow/list/ShoppingListsAdapter.kt
+++ app/src/main/java/pl/wojtach/listazakupow/list/ShoppingListsAdapter.kt
@@ -2,7 +2,6 @@ package pl.wojtach.listazakupow.list
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import pl.wojtach.listazakupow.R
 import pl.wojtach.listazakupow.shared.ShoppingList
@@ -20,9 +19,8 @@ class ShoppingListsAdapter(var shoppingLists: List<ShoppingList>)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingListHolder =
             LayoutInflater.from(parent.context)
-                    .inflate(R.layout.shopping_list_item_view, parent)
-                    .let { it as ShoppingListSmallView }
-                    .let { ShoppingListHolder(it) }
+                    .inflate(R.layout.shopping_list_small_card, parent, false)
+                    .let { ShoppingListHolder(it as ShoppingListSmallView) }
 
 
     override fun getItemCount(): Int = shoppingLists.size
