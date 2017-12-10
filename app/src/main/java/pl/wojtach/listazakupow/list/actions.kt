b56fa7d8eb99237056902b004a38367e038b29f5
Lukasz <circusmagnus@gmail.com>
2017-12-10 16:00:35 +0100

    Add logic for adding list

--- app/src/main/java/pl/wojtach/listazakupow/list/actions.kt
+++ app/src/main/java/pl/wojtach/listazakupow/list/actions.kt
@@ -1,5 +1,6 @@
 package pl.wojtach.listazakupow.list
 
+import android.content.Context
 import kotlinx.android.synthetic.main.shopping_list_item_view.view.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -8,7 +9,7 @@ import java.util.*
  * Created by Lukasz on 10.12.2017.
  */
 
-fun setupShoppingListsAdapter(shoppingLists: List<ShoppingList>, view: ShoppingListsView) =
+fun drawListView(shoppingLists: List<ShoppingList>, view: ShoppingListsView) =
         view.adapter.apply {
             this.shoppingLists = shoppingLists
             notifyDataSetChanged()
@@ -20,3 +21,11 @@ fun drawSmallShoppingListView(shoppingList: ShoppingList, view: ShoppingListSmal
             name.setText(shoppingList.name)
             date.setText(SimpleDateFormat("dd-MM-yyyy").format(Date(shoppingList.timestamp)))
         }
+
+fun addNewShoppingList(oldData: List<ShoppingList>) = ShoppingList(
+        name = "Lista zakupów",
+        timestamp = System.currentTimeMillis()
+).let { listOf(it) + oldData }
+
+fun saveShoppingLists(context: Context, data: List<ShoppingList>) =
+        data.forEach { writeShoppingList(context, it) }
--- app/src/main/java/pl/wojtach/listazakupow/list/stateHandling.kt
+++ app/src/main/java/pl/wojtach/listazakupow/list/stateHandling.kt
@@ -5,16 +5,19 @@ import android.content.Context
 /**
  * Created by Lukasz on 09.12.2017.
  */
-typealias GetShoppingList<T> = ((T) -> List<ShoppingList>)
+typealias GetShoppingLists<T> = ((T) -> List<ShoppingList>)
 
-fun getAllShoppingLists(): GetShoppingList<Context> =
+fun getAllShoppingLists(): GetShoppingLists<Context> =
         {context -> getAllShoppingListsFromSQLite(context) }
 
-fun <T>GetShoppingList<T>.mutate(howToMutate: (List<ShoppingList>) -> List<ShoppingList>) =
+fun <T> GetShoppingLists<T>.mutate(howToMutate: (List<ShoppingList>) -> List<ShoppingList>) =
 { dataSource: T -> howToMutate(this(dataSource))}
 
-fun <T>GetShoppingList<T>.draw() =
-        { dataSource: T, view: ShoppingListsView -> setupShoppingListsAdapter(this(dataSource), view) }
+fun <T> GetShoppingLists<T>.draw() =
+        { dataSource: T, view: ShoppingListsView -> drawListView(this(dataSource), view) }
+
+fun <T> GetShoppingLists<T>.save(saveToPersistence: (List<ShoppingList>) -> Unit) =
+        { dataSource: T -> this.also { saveToPersistence(this(dataSource)) } }
 
 //fun <T>GetShoppingList<T>.filter(shouldPass: (ShoppingListsView) -> Boolean) =
 //        { dataSource: T, view: ShoppingListsView -> if(shouldPass(view)) this(dataSource) else null }
